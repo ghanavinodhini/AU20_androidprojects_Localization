c96cc8f2a8d925bc1c672878a3bc6f845ef7fd6a
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2020-09-16 10:46:35 +0200

    Added button that changes text

--- app/src/main/java/com/example/localization/MainActivity.kt
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -2,14 +2,34 @@ package com.example.localization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingsTextView:TextView
+    var name = "Ghana"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingsTextView= findViewById(R.id.textView)
     }
 
 //Anpass app to swedish and english
     //Anpassa layout-xml, kotlin kod, bilder
     //Added strings.xml greetings text
+
+fun btn_pressed(view: View)
+{
+Log.d("!!!","Button pressed")
+    greetingsTextView.text = getString(R.string.great_text,name)
+}
+
+    //en -  Great Ghana!!
+    //sv - Bra Jobbat Ghana!!
+
+
 }
