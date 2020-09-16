ed9f2fb631e96c53a1b26408973d8ad5eb7b85b9
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2020-09-16 09:33:39 +0200

    Added to do list

---
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+//Anpass app to swedish and english
+    //Anpassa layout-xml, kotlin kod, bilder
+}
